45f30537f8f08b011135a8607b5b2c1a36ca59c9
Jrrdan <jordan.benavent@etu.unice.fr>
2022-03-22 10:02:55 +0100

    Création page accueil #7

--- front-end/src/app/app.routing.module.ts
+++ front-end/src/app/app.routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { QuizListComponent } from './quizzes/quiz-list/quiz-list.component';
 import { EditQuizComponent } from './quizzes/edit-quiz/edit-quiz.component';
 import { UserListComponent } from './users/user-list/user-list.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 const routes: Routes = [
     {path: 'user-list', component: UserListComponent},
     {path: 'quiz-list', component: QuizListComponent},
     {path: 'edit-quiz/:id', component: EditQuizComponent},
-    { path: '', redirectTo: '/quiz-list', pathMatch: 'full' },
+    {path: 'accueil' , component: AccueilComponent },
+    { path: '', redirectTo: '/accueil', pathMatch: 'full' },
 ];
 
 @NgModule({
