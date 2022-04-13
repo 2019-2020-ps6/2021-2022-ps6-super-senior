e6b3cf3d66304bd18f76e9ee9018cc6f529751ed
Louis Hattiger <74056672+Louishattiger@users.noreply.github.com>
2022-04-13 14:45:26 +0200

    Lorsqu'on clique sur protanopie la police devient bleu #12

--- front-end/src/app/accueil/accueil.component.ts
+++ front-end/src/app/accueil/accueil.component.ts
@@ -1,3 +1,4 @@
+import { style } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
@@ -8,9 +9,35 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class AccueilComponent implements OnInit {
 
+  isClick: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  change(): void {
+    //var baseColor = "rgb(0,0,0)";
+    //&& document.body.style.color == baseColor
+    if(this.isClick == false){
+      var x = 0; // range is 0-255
+      var y = 0;
+      var z = 255;
+      var thergb = "rgb(" + x + "," + y + "," + z + ")"; 
+      console.log(thergb,document.body.style.background);
+      document.body.style.color=thergb;
+      this.isClick = true;
+    }
+    else{
+      var x = 0; // range is 0-255
+      var y = 0;
+      var z = 0;
+      var thergb = "rgb(" + x + "," + y + "," + z + ")"; 
+      console.log(thergb,document.body.style.background);
+      document.body.style.color=thergb;
+      this.isClick = false;
+    }
+    
   }
 
 }
